59274b5c0c442a3200bb409b6cec0d79b8a65908
Wubba-jpeg <Pdxfxa@gmail.com>
2026-01-04 21:26:32 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 04, 2026 at 06:30 PM
+-- Generation Time: Jan 04, 2026 at 09:24 PM
 -- Server version: 10.11.14-MariaDB-0+deb12u2
 -- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -69,7 +70,10 @@ CREATE TABLE `levels` (
   `likes` int(11) DEFAULT 0,
   `length` int(11) DEFAULT 0,
   `uploadDate` timestamp NULL DEFAULT current_timestamp(),
-  `featured` int(11) NOT NULL
+  `featured` int(11) NOT NULL,
+  `rated` int(11) NOT NULL DEFAULT 0,
+  `demon` int(11) NOT NULL DEFAULT 0,
+  `auto` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -145,144 +149,6 @@ ALTER TABLE `levels`
 --
 ALTER TABLE `users`
   MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;-- --------------------------------------------------------
-
---
--- Table structure for table `levels`
---
-
-CREATE TABLE `levels` (
-  `levelID` int(11) NOT NULL,
-  `levelName` varchar(255) DEFAULT NULL,
-  `description` text DEFAULT NULL,
-  `levelVersion` int(11) DEFAULT 1,
-  `userID` int(11) DEFAULT NULL,
-  `userName` varchar(255) DEFAULT NULL,
-  `difficulty` int(11) DEFAULT 0,
-  `downloads` int(11) DEFAULT 0,
-  `officialSong` int(11) DEFAULT 0,
-  `gameVersion` int(11) DEFAULT 1,
-  `likes` int(11) DEFAULT 0,
-  `length` int(11) DEFAULT 0,
-  `uploadDate` timestamp NULL DEFAULT current_timestamp(),
-  `featured` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `userId` int(11) NOT NULL,
-  `userName` text NOT NULL,
-  `udid` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `actions`
---
-ALTER TABLE `actions`
-  ADD PRIMARY KEY (`actionID`);
-
---
--- Indexes for table `levels`
---
-ALTER TABLE `levels`
-  ADD PRIMARY KEY (`levelID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `actions`
---
-ALTER TABLE `actions`
-  MODIFY `actionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `levels`
---
-ALTER TABLE `levels`
-  MODIFY `levelID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-  `downloads` int(11) DEFAULT 0,
-  `officialSong` int(11) DEFAULT 0,
-  `gameVersion` int(11) DEFAULT 1,
-  `likes` int(11) DEFAULT 0,
-  `length` int(11) DEFAULT 0,
-  `uploadDate` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `userId` int(11) NOT NULL,
-  `userName` text NOT NULL,
-  `udid` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `levels`
---
-ALTER TABLE `levels`
-  ADD PRIMARY KEY (`levelID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `levels`
---
-ALTER TABLE `levels`
-  MODIFY `levelID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
