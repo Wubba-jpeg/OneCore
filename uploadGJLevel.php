2c56b3fb785e6658cb7c2aff7e82cd5105781e0a
Wubba-jpeg <Pdxfxa@gmail.com>
2026-01-04 09:13:42 +0000

    Optimize level upload process in uploadGJLevel.php

--- uploadGJLevel.php
+++ uploadGJLevel.php
@@ -11,28 +11,27 @@ $levelName = $_POST["levelName"];
 $levelData = $_POST["levelString"];
 $levelDesc = $_POST["levelDesc"];
 $udid = $_POST["udid"];
-$userName = $_POST["userName"];  // Use this directly
+$userName = $_POST["userName"];
 $length = $_POST["levelLength"];
 $song = $_POST["audioTrack"];
 
 
 $uid = $gs->userID($udid, $userName);
 
-// get the level id
+// insert the level metadata into the database
 
-$query = $db->prepare("SELECT MAX(levelID) FROM levels");
+$query = $db->prepare("INSERT INTO levels (levelName, description, userID, userName, length, officialSong, gameVersion, levelVersion, difficulty, downloads, likes, uploadDate, featured) VALUES ('$levelName', '$levelDesc', $uid, '$userName', '$length', '$song', 1, 1, 0, 0, 0, NOW(), 0)");
 $query->execute();
-$result = $query->fetchColumn();
-$levelID = ($result ?: 0) + 1;
 
-// add the string 
+// get the auto generated level id
+
+$levelID = $db->lastInsertId();
+
+// add the level string to a file with the level id
 
 $lvlfile = "levelData/$levelID";
 file_put_contents($lvlfile, $levelData);
 
-// insert
-
-$query = $db->prepare("INSERT INTO levels (levelID, levelName, description, userID, userName, length, officialSong, gameVersion, levelVersion, difficulty, downloads, likes) VALUES ($levelID, '$levelName', '$levelDesc', $uid, '$userName', '$length', '$song', 1, 1, 0, 0, 0)");
-$query->execute();
 echo $levelID;
+?>echo $levelID;
 ?>
