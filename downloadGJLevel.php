6ba07311b6192f308b2c22c485b2e7434c1645df
Wubba-jpeg <Pdxfxa@gmail.com>
2026-01-05 16:24:01 +0000

    forgot to add this for rating.

--- downloadGJLevel.php
+++ downloadGJLevel.php
@@ -28,7 +28,7 @@ $level = $levels[0];
 $levelstring = $leveldat;
 
 // Level Object
-$response = "1:{$level["levelID"]}:2:{$level["levelName"]}:3:{$level["description"]}:5:{$level["levelVersion"]}:6:{$level["userID"]}:8:10:9:{$level["difficulty"]}:10:{$level["downloads"]}:11:0:12:{$level["officialSong"]}:13:{$level["gameVersion"]}:14:{$level["likes"]}:15:{$level["length"]}:4:$levelstring";
+$response = "1:{$level["levelID"]}:2:{$level["levelName"]}:3:{$level["description"]}:5:{$level["levelVersion"]}:6:{$level["userID"]}:8:10:9:{$level["difficulty"]}:10:{$level["downloads"]}:11:0:12:{$level["officialSong"]}:13:{$level["gameVersion"]}:14:{$level["likes"]}:15:{$level["length"]}:18:{$level["rated"]}:19:{$level["featured"]}:17:{$level["demon"]}:25:{$level["auto"]}:4:$levelstring";
 
 echo $response;
 ?>
