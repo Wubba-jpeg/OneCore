a46f0a35fbc8dc5aeb1029cf91fe9ef4840ce487
Wubba-jpeg <Pdxfxa@gmail.com>
2026-01-23 22:01:44 +0000

    Implement cron feature and include misc config
    Added cron functionality to automatically run if activated, and included a new config file for miscellaneous settings.

--- dashboard/rateTool.php
+++ dashboard/rateTool.php
@@ -3,6 +3,7 @@ include "../config/discord.php";
 include "../incl/lib/connection.php";
 include "../config/pass.php";
 include "../incl/lib/mainLib.php";
+include "../config/misc.php";
 require_once "../incl/lib/injectionlibpatch.php";
 require "../incl/lib/DiscordWebhook.php";
 
@@ -135,75 +136,15 @@ if ($webhooks === true) {
     ->setColor("#fffd7a")
     ->send();
 } 
+// run cron if activated
+if ($autocron === true) {
+include "cron.php";
+echo "Cron Ran Automatically!<br>";
+} else {
+echo "Cron hasn't been ran automatically. either do it manually or activate it in misc.php.<br>";
+}
   
   echo "<hr>";
-
-        $message = "you rated the levels";
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>rate tool</title>
-</head>
-<body>
-
-<h2>rating tool</h2>
-
-<?php if (isset($message)) echo "<p>$message</p>"; ?>
-
-<form method="post">
-    <div>
-        <label>password</label><br>
-        <input type="password" name="password">
-    </div>
-
-    <div>
-        <label>level id</label><br>
-        <input type="number" name="levelID">
-    </div>
-
-    <div>
-        <label>stars</label><br>
-        <input type="number" name="stars" min="1" max="10">
-    </div>
-
-    <div>
-        <label>featured put 0 or 1</label><br>
-        <input type="number" name="featured" min="0" max="1">
-    </div>
-
-    <br>
-    <button type="submit">rate the level</button>
-</form>
-
-</body>
-</html>    $difficulty = 0;
-}
-
-    if ($password !== $RATE_PASSWORD) {
-        $message = "wrong password!";
-    } elseif ($levelID <= 0 || $rated <= 0 || $rated > 10) {
-        $message = "invalid input!!!!!!";
-    } else {
-        $stmt = $db->prepare("
-            UPDATE levels
-            SET rated = :rated,
-                featured = :featured,
-                demon = :demon,
-                difficulty = :difficulty
-            WHERE levelID = :levelID
-        ");
-
-        $stmt->execute([
-            ":rated"    => $rated,
-            ":featured" => $featured,
-            ":demon"    => $demon,
-            ":difficulty" => $difficulty,
-            ":levelID"  => $levelID
-        ]);
 
         $message = "you rated the levels";
     }
